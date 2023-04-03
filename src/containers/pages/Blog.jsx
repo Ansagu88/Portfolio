a3b49291fc02545d3233b8221c9bda0e98cb76a5
Oscar Sanchez <ansagu88@gmail.com>
2023-04-03 14:23:10 -0300

    First API request made

--- src/containers/pages/Blog.jsx
+++ src/containers/pages/Blog.jsx
@@ -1,9 +1,25 @@
 import Layout from "hocs/layouts/Layout"
 import Navbar from "components/navigation/Navbar"
 import Footer from "components/navigation/Footer"
+import { useEffect } from "react"
+import { get_categories } from "redux/actions/categories/categories"
+import { connect } from "react-redux"
 
 
-function Blog() {
+function Blog({
+    get_categories,
+    categories
+
+}) {
+
+    useEffect(()=>{
+        window.scrollTo(0,0)
+        get_categories()
+    },[])
+
+
+
+
     return (
         <Layout>
             <Navbar/>
@@ -15,5 +31,10 @@ function Blog() {
         
     )
 }
+const mapStateToProps=state=>({
+    categories: state.categories.categories
+})
 
-export default Blog
+export default connect(mapStateToProps,{
+    get_categories,
+})(Blog)
